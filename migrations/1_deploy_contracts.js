834967d9d711c73661a9f34504ad79de9ac71f7a
Jimmy Teran <jim95.teran@gmail.com>
2023-08-14 17:47:11 -0400

    Adding configuration for deploying contracts to testnet and mainent

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,4 +1,8 @@
+const AttesterResolver = artifacts.require("AttesterResolver");
 
 module.exports = function(deployer) {
-  
+  const eas = '0x1a5650d0ecbca349dd84bafa85790e3e6955eb84'
+  const targetAttester = '0xc3689E0F44672CEC04387d6437968f6ead9d3a09'
+
+  deployer.deploy(AttesterResolver, eas, targetAttester);
 };
