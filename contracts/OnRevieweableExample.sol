967e0a15d71d96f5ea87738a0c62c92f0d67b9d7
Carlos Osiris Lopez <ozzlopz@gmail.com>
2023-09-19 14:12:18 -0600

    fix example reviewable contract

--- contracts/OnRevieweableExample.sol
+++ contracts/OnRevieweableExample.sol
@@ -3,18 +3,22 @@ pragma solidity 0.8.19;
 
 import "./interfaces/IOnReviewable.sol";
 
-
-
 contract OnReviewableExample is IOnReviewable {
     struct Review {
-      address reviewer;
-      string requestName;
-      bytes32 attestationID;
+        address reviewer;
+        string requestName;
+        bytes32 attestationID;
     }
 
     mapping(address => Review) private reviews;
 
-    function onReview(Attestation calldata attestation, string memory requestName) external override {
+    function onReview(Attestation calldata attestation, string memory requestName) external override returns (bool) {
         reviews[attestation.attester] = Review(attestation.attester, requestName, attestation.uid);
+        return true;
+    }
+
+    function getReview(address reviewer) public view returns (string memory requestName, bytes32 attestationID) {
+        Review memory review = reviews[reviewer];
+        return (review.requestName, review.attestationID);
     }
 }
